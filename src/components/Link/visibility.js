e883adee60a588800b8ea7597bf935756dd9541d
Sam Millar <sammillar@mixcloud.com>
2019-11-18 17:41:29 +0000

    Prevent multiple calls

--- src/components/Link/visibility.js
+++ src/components/Link/visibility.js
@@ -4,11 +4,11 @@ import throttle from 'throttle-debounce/throttle';
 const DELAY = 200;
 
 
-export const checkRefVisibility = throttle(DELAY, (refCallbacks, callback) => {
+export const checkRefVisibility = throttle(DELAY, (refProps, callback) => {
     const bottom = window.innerHeight || document.documentElement.clientHeight;
     const right = window.innerWidth || document.documentElement.clientWidth;
 
-    Object.keys(refCallbacks).forEach(ref => {
+    Object.keys(refProps).forEach(ref => {
         const rect = ref.getBoundingClientRect();
         const visible = (
             rect.top >= 0 &&
@@ -17,7 +17,8 @@ export const checkRefVisibility = throttle(DELAY, (refCallbacks, callback) => {
             rect.right <= right
         );
         if (visible) {
-            callback(refCallbacks[ref]);
+            callback(refProps[ref]);
+            delete refProps[ref]; // Only allow this to fire once
         }
     });
 });
